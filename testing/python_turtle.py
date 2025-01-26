6ab92a8b90b18edb00a6fdcc50740c4e7876e03e
Malcolm Krolick <malcolm.krolick@gmail.com>
2025-01-25 20:49:02 -0500

    Make this a game
    - Added randomly spawning targets
    - Added score updating
    - Created turtles plus methods to update

--- testing/python_turtle.py
+++ testing/python_turtle.py
@@ -9,10 +9,23 @@ class TurtleController:
     def __init__(self):
         self.turtle = turtle.Turtle()
         self.turtle.speed(0) 
-        self.turtle.shapesize(2, 2)  # make the turtle bigger
+        self.turtle.shapesize(2, 2)
         self.screen = turtle.Screen()
+        self.screen.setup(600, 600)
         
-        # Garbage and I hate it
+        # Dot writing turtle init
+        self.dot = turtle.Turtle()
+        self.dot.hideturtle()
+        self.dot.penup()
+        self.dot.color('red')
+        
+        # Score display turtle init
+        self.score_display = turtle.Turtle()
+        self.score_display.hideturtle()
+        self.score_display.penup()
+        self.score_display.goto(0, 250)
+        self.score = 0
+        
         self.key_states: Dict[str, bool] = {
             'w': False, 
             's': False,  
@@ -22,6 +35,8 @@ class TurtleController:
         
         self.setup_controls()
         self.screen.listen()
+        self.update_score_display()
+        self.create_new_dot()
         
     def setup_controls(self):
         for key in self.key_states:
@@ -31,6 +46,20 @@ class TurtleController:
 
     def handle_keychange(self, key: str, state: bool):
         self.key_states[key] = state
+
+    def create_new_dot(self):
+        import random
+        x = random.randint(-250, 250)
+        y = random.randint(-250, 250)
+        self.dot.clear()
+        self.dot.goto(x, y)
+        self.dot.dot(20)
+
+    def check_collision(self):
+        if self.turtle.distance(self.dot) < 30:
+            self.score += 1 
+            self.update_score_display()
+            self.create_new_dot()
 
     def update(self):
         # oooga booga - i should use a switch statement - but i dont want to 
@@ -43,12 +72,16 @@ class TurtleController:
         if self.key_states['d']:
             self.turtle.right(self.TURN_ANGLE)
             
+        self.check_collision()
         self.screen.ontimer(self.update, self.UPDATE_INTERVAL)
 
     def run(self):
-        # start game
         self.update()
         turtle.mainloop()
+
+    def update_score_display(self):
+        self.score_display.clear()
+        self.score_display.write(f"Score: {self.score}", align="center", font=("Arial", 16, "normal"))
 
 if __name__ == "__main__":
     controller = TurtleController()
